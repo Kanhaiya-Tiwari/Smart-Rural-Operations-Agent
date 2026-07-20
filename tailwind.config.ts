0828876b604a02c9105b993da6a175fb795a3513
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-07-20 10:57:49 +0530

    Remove unnecessary blank line in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
-
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
